25a4c318842dc653752d1b0046d90e652ba5e2b0
osada47 <251zs02263@student.zen.ac.jp>
2026-02-11 21:17:26 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  const msg = document.getElementById("message");
+
+  msg.textContent = "プログラム実行中…エラー0件！奇跡です！次も頑張ります！！";
+
+  msg.style.color = "green";
+  msg.style.fontSize = "24px";
+}
